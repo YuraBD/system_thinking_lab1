7af38c88606f2ffd15031d3cbee4d0d2ae4b6a40
Yura <yura.bodakovskiy@gmail.com>
2022-10-04 02:03:28 +0300

    Lab1 done

---
+++ data/a.py
@@ -0,0 +1,7 @@
+with open('Lab1/data/deterministic_angles1.csv', 'r') as in_file, open('Lab1/data/deterministic_angles.csv', 'w') as out_file:
+    seen = set() # set for fast O(1) amortized lookup
+    for line in in_file:
+        if line in seen: continue # skip duplicate
+
+        seen.add(line)
+        out_file.write(line)
